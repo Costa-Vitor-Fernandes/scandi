35a341c57907800dc2d3a80e07fa113afbeee23a
VitorCosta <Costa-Vitor-Fernandes>
2022-10-13 22:22:55 -0300

    minicart working(with more than 2 items, .map bug)

--- src/Classes/MiniCart.js
+++ src/Classes/MiniCart.js
@@ -1,8 +1,14 @@
 import { Component } from "react";
 import styled from "styled-components";
 import MiniProduct from "./MiniProduct";
+import {Link} from 'react-router-dom'
 
-const DivFlex =styled.div`
+const Scrollable = styled.div`
+overflow-y:scroll;
+height:80vh;
+`
+
+const DivFlex = styled.div`
 display:flex;
 flex-direction:row;
 justify-content:space-around;
@@ -14,7 +20,7 @@ export default class MiniCart extends Component {
     constructor(props) {
         super(props);
         this.state={
-            cart:[JSON.parse(window.localStorage.getItem('cart'))],
+            cart:JSON.parse(window.localStorage.getItem('cart')),
         }
         
     }
@@ -22,28 +28,30 @@ export default class MiniCart extends Component {
     
 
     render(){
-        console.log(this.state.cart, JSON.parse(window.localStorage.getItem('cart')))
+        // console.log(this.state.cart, JSON.parse(window.localStorage.getItem('cart')))
+        console.log(this.state.cart, 'cartstate')
         if(this.state.cart === null){
 
             return (<div>nothing here
         </div>)
         }
         // this first div must be scrollable and custom scroll
-        return(<div>
+        return(<Scrollable>
             <DivFlex>
             <h1>My bag,</h1><p>{this.state.cart.length}{' '}items</p>
             </DivFlex>
 
             {this.state.cart.map((v,i,arr)=>{
                 // console.log(v)
-                  return  <MiniProduct product={v} currencyIndex={this.props.currencyIndex} currencySymbols={this.props.currencySymbols}></MiniProduct>
+                  return  <MiniProduct key={i} product={arr[i]} currencyIndex={this.props.currencyIndex} currencySymbols={this.props.currencySymbols}>                
+                  </MiniProduct>
             })}
             {/* BUTTONS */}
             <DivFlex>
                 {/* same components here, primary secondary */}
-                <button>view bag</button>
+                <Link to={'/cart'}><button>view bag</button></Link>
                 <button>checkout</button>
             </DivFlex>
-        </div>)
+        </Scrollable>)
     }
 }
