a70712d6d927518e6249e110d2ba2775439c3c11
VitorCosta <Costa-Vitor-Fernandes>
2022-10-18 16:44:48 -0300

    MiniAttribute to show the attributes on mini cart

--- src/Classes/MiniAttribute.js
+++ src/Classes/MiniAttribute.js
@@ -1,10 +1,90 @@
 import { Component } from "react";
+import styled from "styled-components";
+import { Attributes, AttrTitle, AttrButtonsContainer, SelectAttr, SelectColorAttr } from "./AttributesPicker";
+/* {JSON.stringify(this.state.attributes)} */
 
-export default class MiniAttribute extends Component{
+
+const MiniAttrTitle = styled(AttrTitle)`
+font-weight: 400;
+font-size: 14px;`
+
+const MiniAttrButtonsContainer = styled(AttrButtonsContainer)`
+padding:0;
+margin:0;`
+
+const MiniSelectColorAttr = styled(SelectColorAttr)`
+width:16px;
+height:16px;
+padding:0;
+box-shadow: none;
+&:hover{
+    box-shadow:none
+}`
+
+const MiniSelectAttr = styled(SelectAttr)`
+width:24px;
+height:24px;
+padding:0 4px;;
+box-shadow: none;
+&{font-size:12px;}
+&:hover{
+    box-shadow:none;
+}
+`
+const ButtonText = styled.p`
+font-weight: 100;
+font-size: 12px;
+`
+
+
+export default class MiniAttribute extends Component {
+  constructor(props) {
+    super(props);
+    this.selectedAttr = this.selectedAttr.bind(this)
+    this.state = {
+      selectedAttr: this.props.selected,
+      attributes: this.props.attributes,
+    };
+  }
+
+  selectedAttr = (firstId, secondId) =>{
     
-    render(){
-        return (<div>{JSON.stringify(this.props.selected)}
-        </div>)
-    }
+    if(this.state.selectedAttr[firstId] === secondId){
+        return true
+    }   else return false
+  }
 
+  render() {
+    console.log(this.state.attributes, " state attrs");
+    if (!this.state.attributes[0]) return null; 
+    return this.state.attributes.map((v, index, arr) => {
+      if (v.type === "swatch")
+        return (
+          <Attributes>
+            <MiniAttrTitle>{v.id}:</MiniAttrTitle>
+            <AttrButtonsContainer>
+
+            {this.state.attributes[index].items.map((v, i, array) => {
+                return <MiniSelectColorAttr selected={this.selectedAttr(arr[index].id,i)} swatch={v.value}/>;
+            })}
+            </AttrButtonsContainer>
+          </Attributes>
+        );
+      else
+        return (
+          <Attributes>
+            <MiniAttrTitle>{v.id}:</MiniAttrTitle>
+            <AttrButtonsContainer>
+            {this.state.attributes[index].items.map((v, i, array) => {
+                
+              return <MiniSelectAttr selected={this.selectedAttr(arr[index].id, i)}><ButtonText>{v.value}</ButtonText></MiniSelectAttr>;
+            })}
+            </AttrButtonsContainer>
+          </Attributes>
+        );
+    });
+
+    // return (<div> {JSON.stringify(this.state.selectedAttr)}
+    //   </div>)
+  }
 }
